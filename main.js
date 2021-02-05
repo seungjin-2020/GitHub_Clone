a11f01941545607770772cc4c247d657bc2cc900
이승진 <dltmdwls154@gmail.com>
2021-02-06 00:04:33 +0900

    visual add

---
+++ main.js
@@ -0,0 +1,29 @@
+(function(window,document){
+    'use strict';
+
+    const $toggles = document.querySelectorAll('.toggle'); //NodeList 유사배열
+    const $toggleBtn = document.getElementById('toggle-btn');
+
+    $toggleBtn.addEventListener('click',function () {
+        toggleElements();
+    });
+
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 1024) {
+            // Off toogle element
+            offElements();
+        }
+    })
+
+    function toggleElements () {
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.toggle('on');
+        });
+    }
+
+    function offElements () {
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.remove('on');
+        });
+    }
+}) (window,document) 
